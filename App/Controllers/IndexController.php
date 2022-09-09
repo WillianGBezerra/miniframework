b2b7ddc597403232d51d675334900c55bed7bdb5
williangbezerra <williangomesbezerra@gmail.com>
2022-09-08 22:49:12 -0300

    Reutilizando layouts e conexão com DB e Models.

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,17 +2,29 @@
 	namespace App\Controllers;
 
 	use MF\Controller\Action;
+	use App\Connection;
+	use App\Models\Produto;
 
 	class IndexController extends Action {
 
 		public function index() {
-			$this->view->dados = array('sofa', 'cadeira', 'cama');
-			$this->render('index');
+			//$this->view->dados = array('sofa', 'cadeira', 'cama');
+			
+			//instancia de conexão
+			$conn = Connection::getDb();
+
+			//instancia modelo
+			$produto = new Produto($conn);
+			
+			$produtos = $produto->getProdutos();
+
+			$this->view->dados = $produtos;
+			$this->render('index', 'layout3');
 		}
 
 		public function sobreNos() {
 			$this->view->dados = array('Notebook','Smartphone');
-			$this->render('sobreNos');
+			$this->render('sobreNos', 'layout2');
 		}
 	}
 ?>
